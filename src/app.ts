470babdc3266dadee1d58aeb9f62a141b78fa051
naveensing575 <naveensing575@gmail.com>
2025-08-12 23:52:43 +0530

    fix: app code fix

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,32 @@
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
 import authRoutes from './routes/authRoutes'
 import taskRoutes from './routes/taskRoutes'
+import adminRoutes from './routes/adminRoutes'
 import { errorHandler } from './middlewares/errorMiddleware'
-import morgan from 'morgan';
-import cors from 'cors';
-import adminRoutes from './routes/adminRoutes';
 
 const app = express()
-
 app.use(express.json())
-
 
 app.use(
   cors({
-    origin: 'https://streamline-navi.netlify.app/login',
+    origin: 'https://streamline-navi.netlify.app',
     credentials: true,
   })
 )
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(morgan('combined'));
-}
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'production') app.use(morgan('combined'))
 
 app.use('/api/auth', authRoutes)
 app.use('/api/tasks', taskRoutes)
 app.use('/api/admin/users', adminRoutes)
-app.use(errorHandler)
 
 app.get('/', (_req, res) => {
   res.status(200).json({ status: 'API is healthy ✅' })
 })
+
+app.use(errorHandler)
 
 export default app
